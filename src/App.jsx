f1c78aca13c9b76fe9261f3436add81a0adf884d
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-06 23:56:31 +0500

    Bug Fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,37 @@
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 
 
 function App() {
-  const [password , setPassword] = useState("");
-  const [length , setlength] = useState(8);
-  const [numberAllowed , setNumberAllowed] = useState(false);
-  const [charAllowed , setchar] = useState(false);
+  const [password,setPassword] = useState("");
+  const [length,setlength] = useState(8);
+  const [numberAllowed,setNumberAllowed] = useState(false);
+  const [charAllowed,setchar] = useState(false);
 
   let passwordGenerator = useCallback(()=> {
     let pass = "";
     let string = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    if(numberAllowed == true) string += "0123456789"; 
-    if(charAllowed == true) string += "$@_-+=&*#("; 
+    if (numberAllowed == true) { 
+      console.log("Number Allowed: ", pass)
+      string += "0123456789"; 
+    } 
+    if (charAllowed == true) {
+      console.log("Characted Allowed: ",pass)
+      string += "$@_-+=&*#(";
+    }  
 
-    for (let index = 1; index < length; index++){
+    for (let index = 1; index <= length; index++){
       let char = Math.floor(Math.random() * string.length + 1)
-      let pass = string[char]
-1     
+       pass += string.charAt(char)     
     }
     setPassword(pass);
-  }, [length,numberAllowed,charAllowed,setPassword])
+  }, [length, numberAllowed, charAllowed, setPassword])
+ 
+ 
+  useEffect(() => {
+    passwordGenerator()
+
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
+  
 
   return (
     <>
@@ -31,25 +43,27 @@ function App() {
    </div>
    <div className='flex flex-row text-sm gap-x-2'>
     <div className='flex items-center gap-x-1'>
-      <input  className='cursor-pointer' type="range" min={8} max={50}  value={length} onChange={(e) => {
-        setlength(e.target.value)
-      }} />
+            <input className='cursor-pointer' type="range" min={8} max={50} value={length} onChange={(e) => {
+
+              setlength(e.target.value)
+              console.log("Length : ", length)
+
+            }} />
       <label className='text-yellow-400'>Lenght {length}</label>
 
     </div>
     <div className='flex items-center gap-x-1'>
-      <input type="checkbox"  value={numberAllowed} onChange={() => {
-        setNumberAllowed((prev) => {
-          prev = !prev
-        })
+            <input type="checkbox" defaultChecked={numberAllowed} id='numberInput' onChange={() => {
+              setNumberAllowed((value) => value = !value)
+              console.log("is Number Allowed : ", numberAllowed)
       }}/>
       <label className='text-white'>Number</label>
     </div>
     <div className='flex items-center gap-x-1'>
-      <input type="checkbox"  value={charAllowed} onChange={() => {
-        setchar((prev) => {
-          prev = !prev
-        })
+      <input type="checkbox"  defaultChecked={charAllowed} id='numberInput' onChange={() => {
+              setchar((value) => value = !value)
+              console.log("is Character Allowed : ", charAllowed)
+              
       }}/>
       <label className='text-white'>Characters</label>
     </div>
