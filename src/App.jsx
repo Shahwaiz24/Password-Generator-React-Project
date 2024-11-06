5e51e2f1e63d2f0b4f2d94e5118e06b2ab88d30d
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-06 16:04:20 +0500

    Length Input Password Completed

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react'
 
 
 function App() {
+  const [password , setPassword] = useState("");
+  const [length , setlength] = useState(8);
 
   return (
     <>
-      
+ 
+   <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 py-4 bg-gray-700' >
+   <div className='flex flex-row rounded-lg mb-4 overflow-hidden'>
+    <input type="text" readOnly value={password}  placeholder='Secure Password' className='w-full py-1 px-3 outline-none' />
+    <button className='outline-none bg-blue-700 px-3 py-0.5 shrink-0' >copy</button>
+   </div>
+   <div className='flex flex-row text-sm gap-x-2'>
+    <div className='flex items-center gap-x-1'>
+      <input type="range" min={8} max={50} value={length} onChange={(e) => {
+        setlength(e.target.value)
+      }} />
+      <label className='text-yellow-400'>Lenght {length}</label>
+
+    </div>
+   </div>
+
+      </div>
     </>
   )
 }
